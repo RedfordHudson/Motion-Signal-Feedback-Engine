e61b2817b75ce0c6a60ed7dc8b3d04bf575c5b74
RedfordHudson <redfordfoxhudson@gmail.com>
2025-05-22 20:32:17 -0700

    finally! a modular file structure

--- app/main.cpp
+++ app/main.cpp
@@ -1,31 +1,5 @@
 #include <JuceHeader.h>
-#include <iostream>
-
-class MainComponent : public juce::Component
-{
-public:
-    MainComponent()
-    {
-        setSize(800, 600);
-        std::cout << "MainComponent constructor" << std::endl;
-    }
-
-    void paint(juce::Graphics& g) override
-    {
-        g.fillAll(juce::Colours::black);
-        g.setColour(juce::Colours::white);
-        g.setFont(20.0f);
-        g.drawText("Hello World!", getLocalBounds(), juce::Justification::centred, true);
-    }
-
-    void resized() override
-    {
-        // Called when the component is resized
-    }
-
-private:
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
-};
+#include <TestComponent.h>
 
 class MainWindow : public juce::DocumentWindow
 {
@@ -33,7 +7,7 @@ public:
     MainWindow() : DocumentWindow("Test App", juce::Colours::black, DocumentWindow::allButtons)
     {
         setUsingNativeTitleBar(true);
-        setContentOwned(new MainComponent(), true);
+        setContentOwned(new TestComponent(), true);
         centreWithSize(800, 600);
         setVisible(true);
     }

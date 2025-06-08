7a2701089373194e47147b1ba990b74b17cf4004
RedfordHudson <redfordfoxhudson@gmail.com>
2025-06-07 18:10:08 -0700

    migrated to AudioTester

--- TopComponents/AudioTester/AudioTester.cpp
+++ TopComponents/AudioTester/AudioTester.cpp
@@ -1,9 +1,18 @@
 #include "AudioTester.h"
 
-#include <algorithm>
+#include <FrequencyDisplay.h>
+#include <BarDisplay.h>
+#include <BeatDisplay.h>
 
 AudioTester::AudioTester()
+    :frequencyDisplay(std::make_unique<FrequencyDisplay>()),
+    barDisplay(std::make_unique<BarDisplay>()),
+    beatDisplay(std::make_unique<BeatDisplay>(1.0f/8.0f))
 {
+    addAndMakeVisible(*frequencyDisplay);
+    addAndMakeVisible(*barDisplay);
+    addAndMakeVisible(*beatDisplay);
+
     setSize(800, 600);
     setAudioChannels(0, 2); // 0 input channels, 2 output channels
 }
@@ -11,6 +20,29 @@ AudioTester::AudioTester()
 AudioTester::~AudioTester() {
     shutdownAudio();
 }
+
+
+void AudioTester::paint(juce::Graphics& g)
+{
+    // Fill background
+    g.fillAll(juce::Colours::black);
+    
+    // Draw border
+    g.setColour(juce::Colours::grey);
+    g.drawRect(getLocalBounds(), 1);
+}
+
+void AudioTester::resized() {
+    juce::FlexBox flexbox;
+    flexbox.flexDirection = juce::FlexBox::Direction::column;
+
+    for (auto* child : getChildren()) {
+        flexbox.items.add(juce::FlexItem(*child).withFlex(1.0f));
+    }
+
+    flexbox.performLayout(getLocalBounds());
+}
+
 
 void AudioTester::prepareToPlay(int samplesPerBlockExpected, double sampleRate)
 {
--- TopComponents/AudioTester/AudioTester.h
+++ TopComponents/AudioTester/AudioTester.h
@@ -1,7 +1,12 @@
 #pragma once
 
 #include <juce_audio_utils/juce_audio_utils.h>
+#include <juce_gui_basics/juce_gui_basics.h>
 #include <juce_dsp/juce_dsp.h>
+
+class FrequencyDisplay;
+class BarDisplay;
+class BeatDisplay;
 
 class AudioTester : public juce::AudioAppComponent
 {
@@ -15,14 +20,14 @@ public:
     void releaseResources() override {}
 
     // Component overrides
-    void paint(juce::Graphics& g) override {
-        g.fillAll(juce::Colours::black);
-        g.setColour(juce::Colours::white);
-        g.setFont(20.0f);
-        g.drawText("Audio Test", getLocalBounds(), juce::Justification::centred, true);
-    }
-    void resized() override {}
+    void paint(juce::Graphics& g) override;
+    void resized() override;
 
 private:
+
+    std::unique_ptr<FrequencyDisplay> frequencyDisplay;
+    std::unique_ptr<BarDisplay> barDisplay;
+    std::unique_ptr<BeatDisplay> beatDisplay;
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(AudioTester)
 };
